e37760aec201b207fe21f8e336e8928a72e1fcfb
Eduardo Espinal <146891899+dev-espinaledu@users.noreply.github.com>
2024-05-10 21:58:57 +0000

    Actualizar código

--- DOM/function/js/tienda2.js
+++ DOM/function/js/tienda2.js
@@ -25,3 +25,17 @@ document.addEventListener("DOMContentLoaded", function() {
       cartContainer.classList.toggle('show');
     });
   });
+  
+  function vaciarCarrito() {
+    // Obtener la lista de productos del carrito
+    var listaProductos = document.getElementById("lista-productos");
+    // Eliminar todos los elementos de la lista
+    listaProductos.innerHTML = "";
+    // Actualizar el total u otras acciones necesarias
+    actualizarTotal();
+  }
+
+  function actualizarTotal() {
+    document.getElementById("total").innerText = "0";
+
+  }
